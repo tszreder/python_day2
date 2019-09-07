6213c3d74c78ecf282a2a7c2d07a2cea9dcda697
TSzreder <t.szreder@gmail.com>
2019-09-07 14:51:42 +0200

    Dziedziczenie - przykład, opisana klasa admina

--- Day4/heridity.py
+++ Day4/heridity.py
@@ -5,12 +5,12 @@ class User:
         self.permissionSelect = True
 
     def readPost(self):
-        if self.permissionSelect == True:
+        if self.permissionSelect:
             print("READING...")
 
     def __str__(self):
         return  "login: %s password: %s PERMISSIONS: %s"\
-                % (self.login, self.password, "SELECT" if self.permissionSelect else "")
+                % (self.login, self.password, "SELECT " if self.permissionSelect else "")
 
 # moderator dziedziczy po klasie User (ma te same właściwości i metody)
 class Moderator(User):
@@ -20,26 +20,51 @@ class Moderator(User):
         self.permissionInsert = True
 
     def writePost(self):
-        if self.permissionInsert == True:
+        if self.permissionInsert:
             print("WRITING...")
 
     def __str__(self):
-        return super().__str__() + "INSERT" if self.permissionInsert else ""
+        return super().__str__() + "INSERT " if self.permissionInsert else ""
 
+# Admin dziedziczy po klasie Moderator, a pośrednio też User (ma te same właściwości i metody)
+class Admin(Moderator):
+    def __init__(self, login, password):
+        super().__init__(login, password)
+        self.permissionDelete = True
+        self.permissionUpdate = True
+
+    def __str__(self):
+        return super().__str__() + ("DELETE " if self.permissionDelete else "") + ("UPDATE" if self.permissionUpdate else "")
+
+    def deletePost(self):
+        if self.permissionDelete:
+            print("DELETING...")
+
+    def updatePost(self):
+        if self.permissionUpdate:
+            print("UPDATING...")
+
+print("####USER####")
 user = User("user", "user")
 print(user)
 user.readPost()
-
+print("####MODERATOR####")
 moderator = Moderator("moderator", "moderator")
 print(moderator)
 moderator.readPost()
 moderator.writePost()
 
+print("####ADMIN####")
+admin = Admin("admin", "admin")
+print(admin)
+admin.readPost()
+admin.writePost()
+admin.updatePost()
+admin.deletePost()
+admin.permissionDelete = False
+print(admin)
+admin.deletePost()
 
 
 
 
-
-# Admin dziedziczy po klasie Moderator, a pośrednio też User (ma te same właściwości i metody)
-class Admin(Moderator):
-    pass
