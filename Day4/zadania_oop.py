b12c4d63a3f61dab12c20077a19b69bc25079740
TSzreder <t.szreder@gmail.com>
2019-09-07 12:40:21 +0200

    Zadanie 68

--- Day4/zadania_oop.py
+++ Day4/zadania_oop.py
@@ -3,6 +3,8 @@
 # Utwórz klasę Zawodnik zawierającą metodę do obliczania BMI
 # Utwórz obiekt reprezentujący zawodnika
 # Wywołaj metodę klasową, która zwróci wartość BMI
+from random import choice, sample
+
 
 class Player:
     def __init__(self, name, height, weight):
@@ -22,3 +24,30 @@ p1 = Player("Piotr", 180, 80)
 p2 = Player("Jan", 140, 60)
 print(p1)
 print(p2)
+
+# P68
+# Napisz program lotto, który losuje bez zwracania 6 spośród 49 liczb i wypisuje je na ekran.
+# Zmodyfikuj poprzedni program w taki sposób, by wyświetlał wylosowane liczby od najmniejszej do największej.
+
+class Lotto:
+
+    def __init__(self):
+        self.result = []
+
+    def draw(self):
+        self.result = sample(range(50), k = 6)
+        print(self.__str__())
+
+    def sorting(self):
+        self.result.sort(reverse = True)
+        print(self.__str__())
+
+    def __str__(self):
+        return self.result
+
+# losowanie = sample(range(50), k = 6)
+# print(losowanie)
+
+lotto1 = Lotto()
+lotto1.draw()
+lotto1.sorting()
