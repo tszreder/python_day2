b08e216683445aac8b39ad8b52c5ef792b1fd8d6
TSzreder <t.szreder@gmail.com>
2019-09-07 16:19:07 +0200

    wpisana treść zadania P67

---
+++ Day4/Dziekanat.py
@@ -0,0 +1,11 @@
+
+# P67
+# Wykorzystując obiektowość napisz program DZIEKANAT, który:
+# Umożliwia przypisanie ocen do studenta identyfikowanego przez:
+# Nr_indeksu
+# Imię
+# Nazwisko
+# Przechowywanie tych danych w sekwencji i wykonanie takich operacji jak:
+# Wprowadzanie nowego studenta
+# Wyświetlanie wprowadzonych studentów
+# Usuwanie studenta
