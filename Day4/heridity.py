68bf9b3a67994d3bacb4358d0c991ed65282f293
TSzreder <t.szreder@gmail.com>
2019-09-07 14:35:23 +0200

    Dziedziczenie - przykład, opisana klasa moderatora

--- Day4/heridity.py
+++ Day4/heridity.py
@@ -10,17 +10,30 @@ class User:
 
     def __str__(self):
         return  "login: %s password: %s PERMISSIONS: %s"\
-                % (self.login, self.password, "SELECT" if self.permissionSelect == True else "")
+                % (self.login, self.password, "SELECT" if self.permissionSelect else "")
 
 # moderator dziedziczy po klasie User (ma te same właściwości i metody)
 class Moderator(User):
     def __init__(self, login, password):
-        self.login = login
-        self.password = password
+        # wywołanie metody __init__() klasy nadrzędnej
+        super().__init__(login, password)
         self.permissionInsert = True
+
+    def writePost(self):
+        if self.permissionInsert == True:
+            print("WRITING...")
+
+    def __str__(self):
+        return super().__str__() + "INSERT" if self.permissionInsert else ""
 
 user = User("user", "user")
 print(user)
+user.readPost()
+
+moderator = Moderator("moderator", "moderator")
+print(moderator)
+moderator.readPost()
+moderator.writePost()
 
 
 
