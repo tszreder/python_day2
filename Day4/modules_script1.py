01d4b0c9d78d78ad39e534562f82f4ca5d197cd7
TSzreder <t.szreder@gmail.com>
2019-09-08 10:37:35 +0200

    zaległe importowanie modułow

--- Day4/modules_script1.py
+++ Day4/modules_script1.py
@@ -1,3 +1,19 @@
 # import modułu script1
+import random
+import urllib
 
-import Day4.
+# import Day4.modules_script2
+# help(Day4.modules_script2)
+# help(random)
+# print(dir(random))
+
+help(urllib)
+print(dir(urllib))
+
+
+
+# print(Day4.modules_script2.text)
+
+# from Day4.modules_script2 import number, text
+# print(number)
+# print(text)
