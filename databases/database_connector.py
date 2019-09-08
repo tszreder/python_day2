46df24c228d7140f649f750f634edb19291d2d60
TSzreder <t.szreder@gmail.com>
2019-09-08 16:24:15 +0200

    selecty, update, delete insert na bazie z poziomu pythona

--- databases/database_connector.py
+++ databases/database_connector.py
@@ -1,13 +1,28 @@
 import pymysql
 
-
-
 # conn = pymysql.connect("localhost", "tomek", "haslo", "pythondb")
 
 class DatabaseConnector:
     def __init__(self):
         self.loginToDBServer("localhost", "tomek", "haslo", "pythondb")
-        self.selectFromUsers()
+        while True:
+            menu = input("(S) - select, (I) - insert, (D) - delete, (U) - update, (Q) - exit")
+            if menu.upper() == "S":
+                self.selectFromUsers()
+            elif menu.upper() == "I":
+                self.insertIntoUsers(input("Imie"), input("Nazwisko"), input("data urodzenia"), input("pensja"), input("płeć"))
+                self.selectFromUsers()
+            elif menu.upper() == "D":
+                self.deleteFromUsersByID(input("Podaj id rekordu do usunięcia"))
+                self.selectFromUsers()
+            elif menu.upper() == "U":
+                self.updateUsersSalary(input("id: "), input("procent podwyżki: "))
+            elif menu.upper() == "Q":
+                #zamknięcie połączenia z bazą danych
+                self.connect.close()
+                break
+            else:
+                print("Błędny wybór")
 
     def loginToDBServer(self, host, user_login, user_password, db_name):
         try:
@@ -28,5 +43,35 @@ class DatabaseConnector:
             print("| %3d | %15s | %15s | %10s | %15.2f | %1s " %
                   (user[0], user[1], user[2], user[3], user[4], user[5])
                   )
+
+    def insertIntoUsers(self, name, lastname, birthdate, salary, gender):
+        try:
+            self.c.execute("insert into users values (default, %s, %s, %s, %s, %s)",
+                           (name, lastname, birthdate, salary, gender))
+            decision = input("Potwierdź dane (T/N)")
+            if decision.upper() == "T":
+                # przeniesienie danych z bufora do bazy danych
+                self.connect.commit()
+            else:
+                # wycofanie wprowadzonych danych
+                self.connect.rollback()
+        except:
+            print("błąd danych!")
+
+    def deleteFromUsersByID(self, id):
+        try:
+            self.c.execute("delete from users where id = %s", id)
+            self.connect.commit()
+        except:
+            print("Błąd danych")
+
+    def updateUsersSalary(self, id, percent):
+        try:
+            self.c.execute("update users set salary = salary * (1 + %s) where id = %s", (percent, id))
+            self.connect.commit()
+        except:
+            print("Błąd danych")
+
+
 
 DatabaseConnector()
