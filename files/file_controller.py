0d603398298dd84e7220b4df75c34f2b74d77953
TSzreder <t.szreder@gmail.com>
2019-09-17 22:15:16 +0200

    dodanie wyjaśnień do poszczeólnych funkcji

--- files/file_controller.py
+++ files/file_controller.py
@@ -1,7 +1,7 @@
 # utworzenie pliku z uprawnieniami do apisu
 file = open("file1.txt", "w")
 
-print(file.name, file.closed, file.mode)
+# print(file.name, file.closed, file.mode)
 
 #zapis do pliku
 file.write("Pierwszy zapis do pliku\n")
@@ -9,7 +9,7 @@ file.write("Drugi zapis do pliku\n")
 file.writelines(['s1\n', 's2\n'])
 file.close()
 
-print(file.name, file.closed, file.mode)
+# print(file.name, file.closed, file.mode)
 
 fileAppend = open("file2.txt", "a")
 fileAppend.writelines(['1','2','3','4','5','6','7','8\n'])
@@ -18,16 +18,20 @@ fileAppend.close()
 #odczyt z pliku
 fileReader = open("file2.txt", "r")
 # print(fileReader.read())
-print(fileReader.read(10))
-# print(fileReader.readlines())
+print("zawartość pliku")
+# print(fileReader.read(4))
+
+# readlines pozwala zwrócić każdą linię jako element listy
+print(fileReader.readlines())
 
 # sprawdzenie pozycji kursora
+print("pozycja kursora")
 print(fileReader.tell())
 
 
 
 # iterowanie po obiekcie pliku
-for line in fileReader:
-    print(line, end="")
+# for line in fileReader:
+#     print(line, end="")
 
 fileReader.close()
