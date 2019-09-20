c14abfd7fed7e326535d24361deca930da8646c4
TSzreder <t.szreder@gmail.com>
2019-09-20 21:16:11 +0200

    testy połączeń plus nowy sposób formatowania stringów

--- databases/new_connection.py
+++ databases/new_connection.py
@@ -12,6 +12,28 @@ with con:
 
     cur.execute("Select * from users")
     result = cur.fetchall()
+    desc = cur.description
+    print(desc)
     for row in result:
-        print(row)
-        # print("{0} {1} {2} {3}".format(row[0], row[1], row[2]), row[3])
+        # print(row)
+        # print("%s %s %s %s %s %s" % (row[0], row[1], row[2], row[3], row[4], row[5]))
+        print("{0} {2} {1} {3} {4} {5}".format(row[0], row[1], row[2], row[3], row[4],row[5]))
+
+    # cur.execute("Select * from users")
+    # for i in range(len(cur.description)):
+    #     print("Column {}:".format(i + 1))
+    #     desc = cur.description[i]
+    #     print("  column_name = {}".format(desc[0]))
+    #     print("  type = {}".format(desc[1]))
+    #     print("  null_ok = {}".format(desc[6]))
+
+def connectToDatabase():
+    login = input("Podaj login")
+    password = input("Podaj hasło")
+    con = pymysql.connect("localhost", login, password, "pythondb2")
+    cur = con.cursor()
+    cur.execute("select * from users")
+    result = cur.fetchall()
+    print(result)
+
+connectToDatabase()
---
+++ formatting/new_style.py
@@ -0,0 +1,6 @@
+print('{1} {0}'.format('one', 'two'))
+a= " one"
+b = 2
+print("{0:_>10} | {1}".format(a, b))
+print("{0:_^10} | {1}".format(a, b))
+print("{0:_<10} | {1}".format(a, b))
