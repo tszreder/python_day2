06cab50a064ced343e735940216ee4c02d492a5f
TSzreder <t.szreder@gmail.com>
2019-09-21 10:03:38 +0200

    powtórka z katalogów

---
+++ more_files/pliki_katalogi.py
@@ -0,0 +1,9 @@
+import os
+import time
+
+print(os.getcwd())
+
+print(os.listdir('.'))
+print(os.listdir('C:\\Users\\B5400\\PycharmProjects\\Szkolenie_PWN\\kodolamacz_vol4'))
+
+print(path.time.ctime())
